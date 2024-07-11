3b4f3e69335bbf85d5ba0ffb423f837bca218bb5
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2024-07-11 12:00:03 +0100

    show a course

--- src/app/pages/courses/show-course/show-course.component.ts
+++ src/app/pages/courses/show-course/show-course.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CoursesService } from '../../../services/courses.service';
+import { Course } from '../../../models/course';
 
 @Component({
   selector: 'app-show-course',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './show-course.component.html',
   styleUrl: './show-course.component.css'
 })
 export class ShowCourseComponent {
 
-}
+  route = inject(ActivatedRoute)
+
+  coursesService = inject(CoursesService)
+  
+
+    myCourse: Course = {
+      title: '',
+      image: '',
+      price: 0,
+      body: '',
+      active: false
+    }
+
+  ngOnInit(): void {
+    this.route.params.subscribe({
+      next: ({id}) => {
+        console.log(id)
+
+        this.coursesService.one(id).subscribe({
+          next: res => {
+            this.myCourse = res
+          }
+        })
+
+      },
+      error: err => console.log(err)
+    })
+  }
+ }
